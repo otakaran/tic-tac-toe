fcb1b504193b9a6be79dee21973a69f57ff4a868
Otakar Andrysek <otakar@me.com>
2017-02-15 13:27:17 -0800

    Drafted methods

--- game.java
+++ game.java
@@ -31,40 +31,108 @@ public class game
     }
     
     /**
+     * This method will act as a turn for player one
+     */
+    public static String[][] player1Play(String[][] board)
+    {
+        // Do something
+        System.out.println("Player 1 (X) it is your turn.");
+        System.out.println("Enter a location (X,Y) to insert your symbol.");
+        return board;
+    }
+    
+    /**
+     * This method will act as a turn for player two
+     */
+    public static String[][] player2Play(String[][] board)
+    {
+        // Do something
+        System.out.println("Player 2 (X) it is your turn.");
+        System.out.println("Enter a location (X,Y) to insert your symbol.");
+        return board;
+    }
+    
+    /**
+     * This method declares that player one is the winner and ends the game
+     */
+    public static boolean player1Win(boolean gameOn)
+    {
+        System.out.println();
+        System.out.println("Congratulations! Player 1 won the game.");
+        gameOn = false;
+        return gameOn;
+    }
+    
+    /**
+     * This method declares that player two is the winner and ends the game
+     */
+    public static boolean player2Win(boolean gameOn)
+    {
+        System.out.println();
+        System.out.println("Congratulations! Player 2 won the game.");
+        gameOn = false;
+        return gameOn;
+    }
+    
+    /**
+     * This method declares that there is a tie game and ends the game
+     */
+    public static boolean tiedGame(boolean gameOn)
+    {
+        System.out.println();
+        System.out.println("Looks like there was a tie. Play again?");
+        gameOn = false;
+        return gameOn;
+    }
+    
+    /**
+     * This method checks the board for any winning combinations or checks if the board is full
+     */
+    public static int checkBoard(String[][] board, int turn)
+    {
+        // Do something
+        return turn;
+    }
+    
+    /**
      * An example of a method - replace this comment with your own
      */
     public static void main()
     {
         int turn = 0;
+        boolean gameOn = true;
         String[][] board = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
-        while (turn <= 4)
+        while (turn <= 4 && gameOn == true)
         {
+            printBoard(board);
             if (turn == 0)
             {
                 // Player "X" will go
                 board = player1Play(board);
+                turn ++;
             }
             else if (turn == 1)
             {
                 // Player "O" will go
                 board = player2Play(board);
+                turn --;
             }
             else if (turn == 2)
             {
                 // Player "X" wins
-                board = player1Win(board);
+                gameOn = player1Win(gameOn);
             }
             else if (turn == 3)
             {
                 // Player "O" wins
-                board = player2Win(board);
+                gameOn = player2Win(gameOn);
             }
             else if (turn == 4)
             {
                 // There is a tie
-                board = tiedGame(board);
+                gameOn = tiedGame(gameOn);
             }
-            turn ++;
+            turn = checkBoard(board, turn);
         }
         System.out.println("Error 1");
     }
